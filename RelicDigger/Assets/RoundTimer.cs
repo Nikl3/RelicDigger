1d6e55a497726d721067bfd289e19b742fbf8682
Nikl3 <niko.tuuli@hotmail.com>
2018-07-26 12:58:25 +0300

    gitignore add

---
+++ RelicDigger/Assets/RoundTimer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class RoundTimer : MonoBehaviour {
+
+    public float timer;
+    TextMeshProUGUI timerText;
+
+	void Start () {
+        timerText = GetComponent<TextMeshProUGUI>();
+	}
+	
+	void Update () {
+        if (timer > 0) {
+            timer -= Time.deltaTime;
+            timerText.text = timer.ToString("f0");
+        }
+        if (timer <= 0) {
+            //times up screen.set active
+        }
+	}
+}
