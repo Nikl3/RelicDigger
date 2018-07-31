7ac8dc2db7ff51b8e7ceac076202f3a8d741e380
Ari Punavuori <ari.punavuori@gmail.com>
2018-07-31 13:15:27 +0300

    Tile v0.1
    Ei tämäkään ole hyvä

--- RelicDigger/Assets/Gamemanager.cs
+++ RelicDigger/Assets/Gamemanager.cs
@@ -7,12 +7,25 @@ using UnityEngine.UI;
 public class Gamemanager : MonoBehaviour {
 
     public float score;
-
-    public TextMeshProUGUI scoreText;
+    public GameObject sandTilePrefab;
+    public Vector2 tileSize;
+    public int tileRows;
+    public int tileColumns;
+    //public TextMeshProUGUI scoreText;
     LayerMask background;
 
 	void Start () {
-		
+       
+        float firstX = (tileColumns / -2f + 0.5f) * tileSize.x;
+        float firstY = (tileRows / -2f + 0.5f) * tileSize.y;
+
+        for(int i = 0; i < tileColumns; i++) {
+            for (int j = 0; j < tileRows; j++) {
+                GameObject sandTile = Instantiate(sandTilePrefab);
+                Vector3 newPos = new Vector3(firstX + i * tileSize.x, firstY + j * tileSize.y, 0.3f);
+                sandTile.transform.position = newPos;
+            }
+        }
 	}
 	
 	// Update is called once per frame
--- RelicDigger/Assets/Scripts/TouchControl.cs
+++ RelicDigger/Assets/Scripts/TouchControl.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class TouchControl : MonoBehaviour {
 
     Vector3 touchStart;
-
+    
+    public GameObject digMask;
     public float zoomOutMin = 1;
     public float zoomOutMax = 8;
     
@@ -25,22 +26,58 @@ public class TouchControl : MonoBehaviour {
             Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
 
             float prevMagnitude = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+
             float currentMagnitude = (touchZero.position - touchOne.position).magnitude;
 
             float difference = currentMagnitude - prevMagnitude;
 
-            zoom(difference * 0.01f);
-             
-            Vector3 direction = touchStart - Camera.main.ScreenToWorldPoint((touchZero.position + touchOne.position)/2);
+            //if (Mathf.Abs(difference) > 100)
+            //{
+            //    zoom(difference * 0.01f);
+            //}
+            
 
-            Camera.main.transform.position += direction;
+            //if (touchOne.phase == TouchPhase.Moved) {
+            //    GameObject DiggedMask = Instantiate(digMask);
+            //    DiggedMask.transform.position = ((Vector3)(touchZero.position + touchOne.position) / 2);
+            //}
+
+            
+
+        } else if (Input.touchCount == 1) {
+
+            Touch touchZero = Input.GetTouch(0);
+
+            if (touchZero.phase == TouchPhase.Began) {
+                touchStart = Camera.main.ScreenToWorldPoint((Vector3)touchZero.position);
+            }
+
+            if (touchZero.phase == TouchPhase.Moved)
+            {
+                GameObject DiggedMask = Instantiate(digMask);
+                DiggedMask.transform.position = Camera.main.ScreenToWorldPoint((Vector3)touchZero.position) + 0.7f * Vector3.down;
+            }
+
+            //Vector3 direction = touchStart - Camera.main.ScreenToWorldPoint(touchZero.position);
+            //Camera.main.transform.position += direction;
+
+        } else if (Input.GetKey(KeyCode.Mouse0)){
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                touchStart = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            }
+            GameObject DiggedMask = Instantiate(digMask);
+            DiggedMask.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition) + 0.7f * Vector3.down;
+            
         }
+
+
 
         
     }
 
-    void zoom(float increment)
-    {
-        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - increment, zoomOutMin, zoomOutMax);
-    }
+    //void zoom(float increment)
+    //{
+    //    Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - increment, zoomOutMin, zoomOutMax);
+    //}
 }
