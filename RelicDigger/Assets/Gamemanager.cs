957295a648753821053c05c3a40c1cad1e4a22ae
Nikl3 <niko.tuuli@hotmail.com>
2018-07-31 13:31:44 +0300

    bone position remember added to gm

--- RelicDigger/Assets/Gamemanager.cs
+++ RelicDigger/Assets/Gamemanager.cs
@@ -12,7 +12,10 @@ public class Gamemanager : MonoBehaviour {
     public TextMeshProUGUI scoreText;
     LayerMask background;
     RoundTimer rt;
+    public Transform bonePos;
     public GameObject[] bones;
+    Vector3[] OrginalPos;
+    Dictionary<GameObject, Vector3> originalPos;
 
     void UpdateTotalScore() {
         scoreText.text = "score " + totalScore;
@@ -22,8 +25,22 @@ public class Gamemanager : MonoBehaviour {
         background = LayerMask.GetMask("background");
         bones = GameObject.FindGameObjectsWithTag("bone");
         rt = FindObjectOfType<RoundTimer>();
+        OrginalPos = new Vector3[bones.Length];
+        originalPos = new Dictionary<GameObject, Vector3>();
+        
+        // same as below
+        //for (int i=0; i < bones.Length; i++) {
+        //    originalPos.Add(bones[i], bones[i].transform.position);
+        //    //OrginalPos[i] = bones[i].transform.position; //asettaa kaikkien orginalPos talteen
+        //    bones[i].transform.position = new Vector3(Random.Range(-3, 3),0 , Random.Range(-5,5));
+        //}
 
-	}
+        foreach (GameObject bone in bones) {
+            originalPos.Add(bone, bone.transform.position);
+            bone.transform.position = new Vector3(Random.Range(-3, 3), 0, Random.Range(-5, 5));
+
+        }
+    }
 	
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.Mouse0) || Input.touchCount > 0) {
@@ -33,7 +50,10 @@ public class Gamemanager : MonoBehaviour {
                 if (rt.timer > 0) {
                     totalScore += boneClickScore;
                     UpdateTotalScore();
-                    Destroy(hit.collider.gameObject);
+                    //hit.collider.transform.position = bonePos.position;
+                    hit.collider.transform.position = originalPos[hit.collider.gameObject] + new Vector3 (0, 1, 0);
+
+                    //Destroy(hit.collider.gameObject);
                 }
                 }
         }
