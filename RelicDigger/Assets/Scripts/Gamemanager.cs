1169f0451ea2d272f9ab8efd84ad57cebdb73dac
Nikl3 <niko.tuuli@hotmail.com>
2018-08-06 10:11:51 +0300

    glowAnimation fix, now working

--- RelicDigger/Assets/Scripts/Gamemanager.cs
+++ RelicDigger/Assets/Scripts/Gamemanager.cs
@@ -35,7 +35,8 @@ public class Gamemanager : MonoBehaviour {
     public GameObject[] bones;
     Vector3[] OrginalPos;
     Dictionary<GameObject, Vector3> originalPos;
-    Animator skeletonAnimator;
+    public Animator skeletonAnimator;
+    public List<GameObject> bonesFound;
     bool tutorialSeen = false;
     int tutorialTextIndex = 0;
 
@@ -43,16 +44,17 @@ public class Gamemanager : MonoBehaviour {
     LayerMask boneLayer;
     LayerMask tileLayer;
 
-    void Start(){
+
+    void Start() {
         Fabric.EventManager.Instance.PostEvent(bgmAudio);
         float firstX = (tileColumns / -2f + 0.5f) * tileSize.x;
         float firstY = (tileRows / -2f + 0.5f) * tileSize.y;
 
-        for (int i = 0; i < tileColumns; i++){
-            for (int j = 0; j < tileRows; j++){
+        for (int i = 0; i < tileColumns; i++) {
+            for (int j = 0; j < tileRows; j++) {
 
                 var prefab = tilePrefab[Random.Range(0, tilePrefab.Count)];
-                Vector3 newPos = new Vector3(firstX + i * tileSize.x, firstY + j * tileSize.y,0 /*Random.Range(-0.2f,-0.4f)*/);
+                Vector3 newPos = new Vector3(firstX + i * tileSize.x, firstY + j * tileSize.y, 0 /*Random.Range(-0.2f,-0.4f)*/);
                 GameObject sandTile = Instantiate(prefab);
                 sandTile.transform.position = newPos;
                 sandTile.transform.parent = tileFolder.transform;
@@ -66,7 +68,7 @@ public class Gamemanager : MonoBehaviour {
 
         OrginalPos = new Vector3[bones.Length];
         originalPos = new Dictionary<GameObject, Vector3>();
-        skeletonAnimator = FindObjectOfType<Animator>();
+        skeletonAnimator.Play("EndStateClip");
 
         // same as below
         //for (int i=0; i < bones.Length; i++) {
@@ -76,32 +78,26 @@ public class Gamemanager : MonoBehaviour {
         //}
 
 
-        foreach (GameObject bone in bones)
-        {
+        foreach (GameObject bone in bones) {
             originalPos.Add(bone, bone.transform.position);
             bone.transform.position = new Vector3(GoodRandom(-4, 6), GoodRandom(-6, 7), 0);
         }
     }
 
-    
+
 
     void Update() {
 
-        if (!tutorialSeen){
-            if (tutorialTextIndex == 0)
-            {
+        if (!tutorialSeen) {
+            if (tutorialTextIndex == 0) {
                 statusText.text = "Hello\n You wannabe Indiana!\n\nYour job is to find some dinosaur bones.";
                 Time.timeScale = 0;
-                if (Input.GetKeyDown(KeyCode.Mouse0))
-                {
+                if (Input.GetKeyDown(KeyCode.Mouse0)) {
                     tutorialTextIndex = 1;
                 }
-            }
-            else if (tutorialTextIndex == 1)
-            {
+            } else if (tutorialTextIndex == 1) {
                 statusText.text = "But be careful and clear the dust by swiping before picking up the bones or otherwise they will break!\n\nGood Luck!";
-                if (Input.GetKeyDown(KeyCode.Mouse0))
-                {
+                if (Input.GetKeyDown(KeyCode.Mouse0)) {
                     tutorialSeen = true;
                     tutorialTable.SetActive(false);
                     statusText.text = "";
@@ -118,12 +114,11 @@ public class Gamemanager : MonoBehaviour {
 
 
             RaycastHit hit;
-            
-            
-            if (Physics.Raycast(ray, out hit, Mathf.Infinity, boneLayer))
-            {
+
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, boneLayer)) {
                 if (energy > 0) {
-                    
+
                     if (Physics2D.OverlapPoint(ray.origin, tileLayer)) {
                         print("osui hiekkaan");
                         Fabric.EventManager.Instance.PostEvent(boneAudio);
@@ -137,14 +132,14 @@ public class Gamemanager : MonoBehaviour {
                         Fabric.EventManager.Instance.PostEvent(boneAudio);
                         totalScore += boneClickScore;
                         UpdateTotalScore();
-                        ShowSkeleton();
                         //hit.collider.transform.position = bonePos.position;
-                        hit.collider.transform.DetachChildren();
-                        hit.collider.transform.position = originalPos[hit.collider.gameObject] + new Vector3(0, 0, 1);
-                        hit.collider.transform.SetParent(paleSkeleton.transform, true);
                         GameObject go = hit.collider.gameObject;
+                        go.transform.DetachChildren();
+                        go.transform.position = originalPos[hit.collider.gameObject] + new Vector3(0, 0, 1);
+                        bonesFound.Add(go);
                         go.GetComponent<BoxCollider>().enabled = false;
                         go.GetComponent<SpriteRenderer>().sortingOrder = 5;
+                        ShowSkeleton();
                         //Destroy(hit.collider.gameObject);
 
                     }
@@ -152,10 +147,10 @@ public class Gamemanager : MonoBehaviour {
             }
         }
 
-        if (energy > 0){
+        if (energy > 0) {
             energy -= Time.deltaTime;
             energyText.text = "energy " + energy.ToString("f0") + "%";
-        } else{
+        } else {
             if (!gameOver) {
                 Fabric.EventManager.Instance.PostEvent(stopAudio);
                 Fabric.EventManager.Instance.PostEvent(failAudio);
@@ -167,16 +162,16 @@ public class Gamemanager : MonoBehaviour {
 
     }
 
-    public void EnergyEvent(float energyInfo){
-        
+    public void EnergyEvent(float energyInfo) {
+
         energy += energyInfo;
 
-        if (energy > 100){
+        if (energy > 100) {
             energy = 100;
         }
     }
 
-    float GoodRandom(float min, float max){
+    float GoodRandom(float min, float max) {
         float average = (min + max) / 2;
         float halfDistance = (max - min) / 2;
         float rng = 1 - Mathf.Pow(Random.value, 2f);
@@ -184,21 +179,20 @@ public class Gamemanager : MonoBehaviour {
 
     }
 
-    void UpdateTotalScore(){
+    void UpdateTotalScore() {
         scoreText.text = "score " + totalScore;
     }
 
-    void ShowSkeleton(){
+    void ShowSkeleton() {
         //paleSkeleton.SetActive(true);
         //if (SkeletonTimer < 0) {
         //}
 
         skeletonAnimator.Play("boneGlow");
 
-        foreach (Animator animator in skeletonAnimator.GetComponentsInChildren<Animator>()) {
-            animator.Play("boneGlow");
+        foreach (GameObject bone in bonesFound) {
+            bone.GetComponent<Animator>().Play("boneGlow");
         }
         //print("skeleton näkyi");
     }
-
 }
